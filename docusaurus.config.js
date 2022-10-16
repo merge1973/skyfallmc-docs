92938015fc30277526ea257263a9d5a405deebdb
merge1973 <ethanchey@outlook.com>
2022-10-16 18:17:40 -0400

    Modified files according to docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,6 +18,8 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'merge1973', // Usually your GitHub org/user name.
   projectName: 'skyfallmc-docs', // Usually your repo name.
+  trailingSlash: false,
+  deploymentBranch: 'gh-pages',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
