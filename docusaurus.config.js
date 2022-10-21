e54cd61a5ae25e2263aec5f07dac953a50d38908
merge1973 <ethanchey@outlook.com>
2022-10-21 01:10:23 -0400

    Use custom css

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,16 +41,16 @@ const config = {
           editUrl:
             'https://github.com/merge1973/skyfallmc-docs/tree/main/',
         },
-        blog: false//{
+        blog: false,//{
           //showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           //editUrl:
             //'https://github.com/merge1973/skyfallmc-docs/tree/main/',
         //},
-        //theme: {
-          //customCss: require.resolve('./src/css/custom.css'),
-        //},
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
       }),
     ],
   ],
