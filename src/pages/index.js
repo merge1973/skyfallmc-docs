2be7f4775fadbe89121c45be51765090027b6d6e
merge1973 <ethanchey@outlook.com>
2022-10-21 17:21:24 -0400

    Add image

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,22 @@ import clsx from 'clsx';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import styles from '../css/index.module.css';
+import ThemedImage from '@theme/ThemedImage';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx(styles.heroBanner)}>
       <div className="container">
+      <ThemedImage
+        alt="Docusaurus themed image"
+        width="50%"
+        sources={{
+          light: useBaseUrl('/img/logo.svg'),
+          dark: useBaseUrl('/img/logodark.svg'),
+        }}
+      />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <a href="/docs"><button className={clsx(styles.button)}>View the Docs →</button></a>
